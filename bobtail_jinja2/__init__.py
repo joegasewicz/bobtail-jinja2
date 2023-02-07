70c33ef6a7aa4217d834a360e296f54a0d3ebf5e
joegasewicz <joegasewicz@gmail.com>
2023-02-07 15:58:28 +0000

    init

---
+++ bobtail_jinja2/__init__.py
@@ -0,0 +1,1 @@
+from bobtail_jinja2.templating import BobtailJinja2
---
+++ bobtail_jinja2/templating.py
@@ -0,0 +1,37 @@
+from typing import Dict
+from bobtail import Request, Response, Tail
+from bobtail.middleware import AbstractMiddleware
+from jinja2 import Environment, FileSystemLoader
+
+
+class _Template:
+
+    env: Environment
+
+    def __init__(self, template_dir: str):
+        file_loader = FileSystemLoader(template_dir)
+        self.env = Environment(loader=file_loader)
+
+    def render(self, res: Response, template: str, *, data: Dict = {}):
+        res.set_headers({"Content-Type": "text/html"})
+        template = self.env.get_template(template)
+        template_str = template.render(**data)
+        res.set_html(template_str)
+
+
+class BobtailJinja2(AbstractMiddleware):
+
+    template_dir: str
+
+    def __init__(self, *, template_dir: str = "templates"):
+        self.template_dir = template_dir
+
+    def run(self, req: Request, res: Response, tail: Tail) -> None:
+        """
+        :param req:
+        :param res:
+        :param tail:
+        :return:
+        """
+        setattr(res, "jinja2", _Template(self.template_dir))
+        tail(req, res)
